e154218ef8205f91764e3055cbdd7eb7c682c41c
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-11-26 15:10:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@ int main() {
     int a=0,b=1,n=0;
     cin>>n;
     if(n>=1){
-    	cout<<b<<endl;
+        cout<<b<<endl;
         while(true){
             if((a+b)<=n){
                 cout<<a+b<<endl;
                 if(a<b){a+=b;}
                 else{b+=a;}
             }else{break;}  	
-	}
+        }
+    }
     return 0;
 }

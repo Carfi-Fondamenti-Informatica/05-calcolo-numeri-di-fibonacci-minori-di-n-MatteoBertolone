f2176b5a6466bcbb60ef22856c5025dc982ff06b
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-11-26 14:02:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+int a=0,b=1,n=0;
+    cin>>n;
+    if(n>=1){
+    		cout<<b<<endl;
+        	while(true){
+             	if((a+b)<=n){
+                    	cout<<a+b<<endl;
+                    	if(a<b){a+=b;}
+				else{b+=a;}
+             	}else{break;}
+      }  	
+	return 0;
+    }
 }
